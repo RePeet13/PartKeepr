728c34bd6218ed06fed727a27cbe4cdbcf633728
Taylor Peet <repeet13@gmail.com>
2013-12-13 09:49:01 -0500

    added some light graphics and paired that with a few stubs

---
+++ pkpr/src/com/unrulyrecursion/partkeeprconnector/AboutFragment.java
@@ -0,0 +1,36 @@
+package com.unrulyrecursion.partkeeprconnector;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+
+public class AboutFragment extends DialogFragment{
+	
+	 @Override
+	    public Dialog onCreateDialog(Bundle savedInstanceState) {
+	        // Use the Builder class for convenient dialog construction
+		
+	        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	        
+	        //Get the layout inflater
+	        LayoutInflater inflater = getActivity().getLayoutInflater();
+	        
+	        //Set the view
+	        builder.setView(inflater.inflate(R.layout.dialog_about, null));
+	        
+	        builder//.setMessage(R.string.about_us)
+	               .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+	                   public void onClick(DialogInterface dialog, int id) {
+	                   }
+	               });
+	              
+	        // Create the AlertDialog object and return it
+	        return builder.create();
+	    }
+
+
+}
---
+++ pkpr/src/com/unrulyrecursion/partkeeprconnector/LoginFragment.java
@@ -0,0 +1,7 @@
+package com.unrulyrecursion.partkeeprconnector;
+
+import android.support.v4.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+}
